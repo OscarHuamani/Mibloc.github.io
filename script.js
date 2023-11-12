bc1ebf7bb9e5ca309502ad40990d89e507c02289
OscarHuamani <oscar1234huamani@gmail.com>
2023-11-12 16:55:44 -0500

    cambios

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Puedes agregar más proyectos aquí
-    const proyectos = [
-        { titulo: 'Desarrollo de Página Web de Registro de Asistencias', descripcion: 'Descripción del proyecto...' },
-        { titulo: 'Desarrollo de Página Web sobre Abogados con WordPress y Divi', descripcion: 'Descripción del proyecto...' },
-        { titulo: 'Desarrollo de Aplicación Móvil con Flutter', descripcion: 'Descripción del proyecto...' }
-    ];
+// script.js
 
-    const proyectosContainer = document.getElementById('proyectos-container');
-
-    proyectos.forEach(proyecto => {
-        const proyectoDiv = document.createElement('div');
-        proyectoDiv.classList.add('project');
-
-        const titulo = document.createElement('h2');
-        titulo.textContent = proyecto.titulo;
-
-        const descripcion = document.createElement('p');
-        descripcion.textContent = proyecto.descripcion;
-
-        proyectoDiv.appendChild(titulo);
-        proyectoDiv.appendChild(descripcion);
-
-        proyectosContainer.appendChild(proyectoDiv);
-    });
-});
+function redirectToProjectsPage() {
+    window.location.href = "Mis Proyectos.html";
+}
